daeddff14989515322a772aa0b6c8dfc1389c0e2
Jack Xu <jackxu@jackdembp.dev.oanda.com>
2014-03-12 11:55:00 -0400

    Initial Commit

---
+++ UWCourse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UWCourse
+//
+//  Created by Jack Xu on 3/12/14.
+//  Copyright (c) 2014 Jack's App Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UWCourse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UWCourse
+//
+//  Created by Jack Xu on 3/12/14.
+//  Copyright (c) 2014 Jack's App Factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
